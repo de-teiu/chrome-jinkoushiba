f6bef6b1792771038baa9ee3d34ba8e081358ba2
LAPTOP-SGEIJU4B\tekur <tekuragari@gmail.com>
2017-09-16 12:54:35 +0900

    contributions in the last yearの文言も書き換えてみた

--- jinkoushiba.js
+++ jinkoushiba.js
@@ -5,4 +5,10 @@ $(function () {
     $('.day').each(function () {
         $(this).attr('fill', colorCdList[Math.floor(Math.random() * colorCdList.length)]);
     });
+
+    $('.f4.text-normal.mb-2').each(function () {
+        if ($(this).text().match(/contributions/)) {
+            $(this).text('A lot of contributions in the last year');
+        }
+    });
 });
