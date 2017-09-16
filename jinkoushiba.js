da0aee2efd30b6aed5bd226a5881ccc7fd816ed0
LAPTOP-SGEIJU4B\tekur <tekuragari@gmail.com>
2017-09-16 10:55:19 +0900

    もうできた

--- jinkoushiba.js
+++ jinkoushiba.js
@@ -1,4 +1,8 @@
 $(function () {
     'use strict';
+    var colorCdList = ['#c6e48b', '#7bc96f', '#239a3b', '#196127'];
 
+    $('.day').each(function () {
+        $(this).attr('fill', colorCdList[Math.floor(Math.random() * colorCdList.length)]);
+    });
 });
