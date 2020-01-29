73f6bb23fb3a84766f4e3067f5c3835090ffc3a8
LAPTOP-SGEIJU4B\tekur <tekuragari@gmail.com>
2020-01-29 23:33:27 +0900

    リリース用に調整

--- jinkoushiba.js
+++ jinkoushiba.js
@@ -1,14 +1,11 @@
-$(function () {
-    'use strict';
-    var colorCdList = ['#c6e48b', '#7bc96f', '#239a3b', '#196127'];
-
-    $('.day').each(function () {
-        $(this).attr('fill', colorCdList[Math.floor(Math.random() * colorCdList.length)]);
+window.onload = () => {
+    const colorCodes = ['#c6e48b', '#7bc96f', '#239a3b', '#196127'];
+    document.querySelectorAll('.day').forEach(element => {
+        element.setAttribute('fill', colorCodes[Math.floor(Math.random() * colorCodes.length)]);
     });
-
-    $('.f4.text-normal.mb-2').each(function () {
-        if ($(this).text().match(/contributions/)) {
-            $(this).text('A lot of contributions in the last year');
+    document.querySelectorAll('.f4.text-normal.mb-2').forEach(element => {
+        if (element.textContent.match(/contributions/)){
+            element.textContent = 'So many contributions in the last year!!!!!!!!!!!';
         }
     });
-});
+};
